2aa2898e8eced7757f725ca88c7896110387e35f
Michael Maurer <mjmaurer777@gmail.com>
2022-07-02 16:16:15 -0400

    Update deps

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@ with import <nixpkgs> { };
 
 stdenv.mkDerivation {
   name = "node";
-  buildInputs = [ nodejs-14_x nodePackages.npm-check-updates nodePackages.serve yarn ];
+  buildInputs = [ nodejs-18_x nodePackages.npm-check-updates nodePackages.serve yarn ];
 }
